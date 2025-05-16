4583f24792de7fbfa3ee9204cf73c0275893172b
PoLin <oliver590617@gmail.com>
2025-05-16 22:17:53 +0800

    feat: add IsTracked property to JointModel for tracking state

--- Assets/KinectManager/Model/JointModel.cs
+++ Assets/KinectManager/Model/JointModel.cs
@@ -5,10 +5,12 @@ public class JointModel
 {
     public Vector3 Position { get; private set; }
     public JointType Type { get; private set; }
+    public bool IsTracked { get; private set; }
 
     public JointModel(Windows.Kinect.Joint joint)
     {
         Type = joint.JointType;
         Position = new Vector3(joint.Position.X, joint.Position.Y, joint.Position.Z);
+        IsTracked = (joint.TrackingState == TrackingState.Tracked);
     }
 }
