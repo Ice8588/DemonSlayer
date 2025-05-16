b6b79f0e62188952e3ffb6cb8b75a3940f7a9e38
PoLin <oliver590617@gmail.com>
2025-05-16 22:18:22 +0800

    feat: enhance BodyState class with elbow tracking and improved debug information

--- Assets/KinectManager/BodyState.cs
+++ Assets/KinectManager/BodyState.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Kinect = Windows.Kinect;
 
@@ -61,13 +62,31 @@ public class BodyState : MonoBehaviour
                         JointModel spineBase = new JointModel(body.Joints[Kinect.JointType.SpineBase]);
                         JointModel neck = new JointModel(body.Joints[Kinect.JointType.Neck]);
                         JointModel head = new JointModel(body.Joints[Kinect.JointType.Head]);
+                        JointModel elbowLeft = new JointModel(body.Joints[Kinect.JointType.ElbowLeft]);
+                        JointModel elbowRight = new JointModel(body.Joints[Kinect.JointType.ElbowRight]);
+
+                        JointModel[] joints = new JointModel[]
+                        {
+                            chest,
+                            hadnLeft,
+                            hadnRight,
+                            shoulderLeft,
+                            shoulderMid,
+                            shoulderRight,
+                            spineBase,
+                            neck,
+                            head,
+                            elbowLeft,
+                            elbowRight
+                        };  
+
+                        debugFunction(joints);
 
                         // Detect the body direction
-                        DetectCatch(hadnLeft, hadnRight);
+                        DetectCatch(hadnLeft, hadnRight, elbowLeft, elbowRight);
                         DetectBodyDirection(head, neck, spineBase);
                         DetectHandDegree(chest, hadnLeft, hadnRight);
-                        DetectTurn(shoulderLeft, shoulderMid, shoulderRight);
-
+                        DetectTurn(elbowLeft, elbowRight);
 
                         break;
                     }
@@ -77,24 +96,37 @@ public class BodyState : MonoBehaviour
         
     }
 
-    void debugFunction()
+    void debugFunction(JointModel[] joints)
     {
-        GameObject.Find("State").GetComponent<TextMesh>().text = "Hand Degree: " + _handDegree + "\n" +
-        "Move H: " + _moveH + "\n" + "Move V: " + _moveV + "\n" +
-        "Turn: " + _turn + "\n" + "Is Catch: " + _isCatch + "\n" +
-        "Is Detect: " + _detectBodyDirection + "\n";
+        string debugString = "";
+        foreach (JointModel joint in joints)
+        {
+            debugString += joint.Type + ": " + joint.Position + "," +joint.IsTracked+"\n";
+        }
+
+        GameObject.Find("KinectState").GetComponent<TextMesh>().text = debugString;
+
+        string debugString2 = "";
+        debugString2 += "isCatch: " + _isCatch + "\n";
+        debugString2 += "moveH: " + _moveH + "\n";
+        debugString2 += "moveV: " + _moveV + "\n";
+        debugString2 += "turn: " + _turn + "\n";
+        debugString2 += "handDegree: " + _handDegree + "\n";
+        GameObject.Find("BodyState").GetComponent<TextMesh>().text = debugString2;
     }
 
 
-    void DetectCatch(JointModel hadnLeft, JointModel hadnRight)
+
+    void DetectCatch(JointModel hadnLeft, JointModel hadnRight, JointModel elbowLeft, JointModel elbowRight)
     {
         _isCatch = Math.Abs(hadnLeft.Position.x - hadnRight.Position.x) < 0.125f &&
-                   Math.Abs(hadnLeft.Position.y - hadnRight.Position.y) < 0.125f;
+                   Math.Abs(hadnLeft.Position.y - hadnRight.Position.y) < 0.125f &&
+                   elbowLeft.IsTracked && elbowRight.IsTracked;
     }
-    
+
     void DetectHandDegree(JointModel chest, JointModel handLeft, JointModel handRight)
     {
-        if(!_isCatch) return;
+        if (!_isCatch) return;
 
         Vector2 handAverage = new Vector2(
         (handLeft.Position.x + handRight.Position.x) / 2,
@@ -103,20 +135,23 @@ public class BodyState : MonoBehaviour
 
         Vector2 chestPosition = new Vector2(chest.Position.x, chest.Position.y);
 
-        float degree = Mathf.Atan2(handAverage.y - chestPosition.y, handAverage.x - chestPosition.x) * Mathf.Rad2Deg;
+        float degree = Mathf.Atan2(handAverage.y - chestPosition.y, handAverage.x - chestPosition.x) * Mathf.Rad2Deg - 90;
 
         if (Vector2.Distance(handAverage, chestPosition) < 0.2f) return;
-        
+
         _handDegree = degree < 0 ? 360 + degree : degree;
     }
 
-    void DetectTurn(JointModel shoulderLeft, JointModel shoulderMid, JointModel shoulderRight)
+    void DetectTurn(JointModel elbowLeft, JointModel elbowRight)
     {
-        float angle = Mathf.Atan2(shoulderLeft.Position.z - shoulderMid.Position.z, shoulderLeft.Position.x - shoulderMid.Position.x) * Mathf.Rad2Deg;
-        angle = angle < 0 ? 360 + angle : angle;
-        
-        if (angle >= 90 && angle <= 150) _turn = -1;
-        else if (angle >= 245 && angle <= 280) _turn = 1;
+        if( _isCatch)
+        {
+            _turn = 0;
+            return;
+        }
+
+        if (!elbowLeft.IsTracked) _turn = -1;
+        else if (!elbowRight.IsTracked) _turn = 1;
         else _turn = 0;
     }
 
