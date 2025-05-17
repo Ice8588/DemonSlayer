5565b914bbd89e109d1f9fdf54fdd51432d6e3bb
XdhanN <hanhanlee0510@gmail.com>
2025-05-18 00:23:16 +0800

    update ui
    update spawner to connect enemy

--- Assets/script/QuadrantHintSpawner.cs
+++ Assets/script/QuadrantHintSpawner.cs
@@ -1,46 +1,57 @@
-using UnityEngine;
-using System.Collections;
-using System;
+//using UnityEngine;
 
-public class QuadrantHintSpawner : MonoBehaviour
-{
-    public QuadrantHint[] quadrantHints;
-    public float minDelay = 2f;
-    public float maxDelay = 8f;
-    public int maxActiveHints = 3;
-    void Start()
-    {
-        foreach (QuadrantHint hint in quadrantHints)
-        {
-            hint.gameObject.SetActive(false);
-            StartCoroutine(SpawnHintLoop(hint));
-        }
-    }
+//public class QuadrantHintSpawner : MonoBehaviour
+//{
+//    [Header("象限 Hint 物件（依照方向順序排列）")]
+//    public QuadrantHint[] quadrantHints;
 
-    IEnumerator SpawnHintLoop(QuadrantHint hint)
-    {
-        while (true)
-        {
-            float delay = UnityEngine.Random.Range(minDelay, maxDelay);
-            yield return new WaitForSeconds(delay);
+//    //private IDirectionProvider directionProvider;
 
-            if (!hint.gameObject.activeSelf && GetActiveHintCount() < maxActiveHints)
-            {
-                // 隨機設定紅或黃事件
-                hint.hintType = (UnityEngine.Random.value > 0.5f) ? HintType.Red : HintType.Yellow;
-                hint.gameObject.SetActive(true);
-            }
-        }
-    }
+//    // 外部注入敵人提供的方向資訊
+//    //public void InjectDirectionProvider(IDirectionProvider provider)
+//    //{
+//    //    directionProvider = provider;
+//    //}
 
-    int GetActiveHintCount()
-    {
-        int count = 0;
-        foreach (QuadrantHint hint in quadrantHints)
-        {
-            if (hint.gameObject.activeSelf)
-                count++;
-        }
-        return count;
-    }
-}
+//    /// 從 directionProvider 取得資料並生成象限提示
+//    void Update()
+//    {
+//        if (directionProvider == null) return;
+
+//        DirectionType[] types = directionProvider.GetAllDirectionTypes();
+
+//        for (int i = 0; i < types.Length && i < quadrantHints.Length; i++)
+//        {
+//            if (types[i] != DirectionType.None && !quadrantHints[i].gameObject.activeSelf)
+//            {
+//                SpawnHint(i, types[i]);
+//            }
+//        }
+//    }
+
+//    /// 在指定方向生成一個象限提示
+//    private void SpawnHint(int index, DirectionType dirType)
+//    {
+//        if (index < 0 || index >= quadrantHints.Length)
+//            return;
+
+//        var hint = quadrantHints[index];
+
+//        if (!hint.gameObject.activeSelf)
+//        {
+//            hint.hintType = ConvertToHintType(dirType);
+//            hint.gameObject.SetActive(true);
+//        }
+//    }
+
+//    /// DirectionType 轉換為 HintType
+//    private HintType ConvertToHintType(DirectionType type)
+//    {
+//        switch (type)
+//        {
+//            case DirectionType.WeakPoint: return HintType.Yellow;
+//            case DirectionType.AttackPoint: return HintType.Red;
+//            default: return HintType.None;
+//        }
+//    }
+//}
